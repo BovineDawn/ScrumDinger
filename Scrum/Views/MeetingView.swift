29a85c3dad6c8ddd79087a8ceb5ed0de59db64f5
Justin <phoenicianorganics@gmail.com>
2025-07-30 15:40:47 -0700

    Added AVFoundation and a ring sound when the timer runs out.

--- Scrum/Views/MeetingView.swift
+++ Scrum/Views/MeetingView.swift
@@ -6,44 +6,61 @@
 //
 
 import SwiftUI
-
+import TimerKit
+import AVFoundation
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @State var scrumTimer = ScrumTimer()
+    
+    private let player = AVPlayer.dingPlayer()
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+//                ProgressView(value: 5, total: 15)
+//                HStack {
+//                    VStack(alignment: .leading) {
+//                        Text("Seconds Elapsed")
+//                            .font(.caption)
+//                        Label("300", systemImage: "hourglass.tophalf.fill")
+//                    }
+//                    Spacer()
+//                    VStack(alignment: .trailing) {
+//                        Text("Seconds Remaining")
+//                            .font(.caption)
+//                        Label("600", systemImage: "hourglass.bottomhalf.fill")
+//                    }
+//                }
+//                .accessibilityElement(children: .ignore)
+//                .accessibilityLabel("Time remaining")
+//                .accessibilityValue("10 minutes")
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
                 }
             }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time remaining")
-            .accessibilityValue("10 minutes")
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
+            .padding()
+            .foregroundColor(scrum.theme.accentColor)
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendeeNames: scrum.attendees.map { $0.name})
+                scrumTimer.speakerChangedAction = {
+                    player.seek(to: .zero)
                 }
-                .accessibilityLabel("Next Speaker")
+                scrumTimer.startScrum()
+            }
+            .onDisappear {
+                scrumTimer.stopScrum()
             }
         }
-        .padding()
     }
-}
 
 
 #Preview {
-    MeetingView()
+    @Previewable @State var scrum = DailyScrum.sampleData[0]
+    MeetingView(scrum: $scrum)
 }
